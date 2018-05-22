7c91d0ee649006b012be918d2122e66d51e5dc6b
darkling6667 <35731422+darkling66@users.noreply.github.com>
2018-05-22 14:04:30 +0300

    Update lab62.cpp

--- lab6/lab62/lab62.cpp
+++ lab6/lab62/lab62.cpp
@@ -1,8 +1,9 @@
 // lab62.cpp : Defines the entry point for the console application.
 //
 
+Отключить для языка: английский
 #include "stdafx.h"
-
+#include "windows.h"
 
 #define FILE(path, file, mode)\
 FILE* file;\
@@ -11,26 +12,13 @@ file = fopen (path, mode);\
 		printf("Error");\
 		return -1;\
 	}
-#define COPY(file, dest, buf, b)\
-while (!feof(file)) {\
-	b = fgets(buf, sizeof(buf), file);\
-	if (b == NULL) {\
-		if (feof(file) != 0) {\
-			printf("Read ends\n");\
-			break;\
-		}\
-		else {\
-			printf("Error\n");\
-			break;\
-		}\
-	}\
-	fprintf(dest, "%s", buf);\
-}
+
+int copy(FILE*, FILE*, char[], char[]);
 
 int main()
 {
 	char a[512];
-	char *b;
+	char *b = 0;
 	char file1[] = "C:\\Users\\User\\source\\repos\\lab 6\\lab 6_2\\file1.txt";
 	char file2[] = "C:\\Users\\User\\source\\repos\\lab 6\\lab 6_2\\file2.txt";
 	char file3[] = "C:\\Users\\User\\source\\repos\\lab 6\\lab 6_2\\file3.txt";
@@ -39,9 +27,9 @@ int main()
 	FILE(file2, fp2, "r");
 	FILE(file3, fp3, "r");
 	FILE(file4, fp4, "w");
-	COPY(fp1, fp4, a, b);
-	COPY(fp2, fp4, a, b);
-	COPY(fp3, fp4, a, b);
+	copy(fp1, fp4, a, b);
+	copy(fp2, fp4, a, b);
+	copy(fp3, fp4, a, b);
 	fclose(fp1);
 	fclose(fp2);
 	fclose(fp3);
@@ -49,3 +37,20 @@ int main()
 	system("pause");
 	return 0;
 }
+
+int copy(FILE* file, FILE* dest, char buf[], char b[]) {
+	while (!feof(file)) {
+		b = fgets(buf, sizeof(buf), file);
+		if (b == NULL) {
+			if (feof(file) != 0) {
+				printf("Read ends\n");
+				break;
+			}
+			else {
+				printf("Error\n");
+				break;
+			}
+		}
+		fprintf(dest, "%s", buf);
+	}
+	return 0;
